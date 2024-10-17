5568eb2068f003dd4b65816f7b0d354753b53c18
Segward <gustask@stud.ntnu.no>
2024-10-17 10:27:40 +0200

    Updated README and made resources

--- src/test/java/edu/ntnu/idi/idatt/MainTest.java
+++ src/test/java/edu/ntnu/idi/idatt/MainTest.java
@@ -1,38 +1,15 @@
 package edu.ntnu.idi.idatt;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class MainTest {
-  public static final String ANSI_RESET = "\u001B[0m";
-  public static final String ANSI_GREEN = "\u001B[32m";
-  public static final String ANSI_YELLOW = "\u001B[33m";
-  public static final String ANSI_RED = "\u001B[31m";
-
-  public static String start() {
-    return ANSI_YELLOW + "[TEST] " + ANSI_RESET;
-  }
-
-  public static String success() {
-    return ANSI_GREEN + " [SUCCESS] " + ANSI_RESET;
-  }
-
-  public static String fail() {
-    return ANSI_RED + " [FAIL] " + ANSI_RESET;
-  }
-
-  public static String compare(boolean condition) {
-    return condition ? success() : fail();
-  }
 
   @Test
   public void testAdd() {
     Main main = new Main();
     int expected = 5;
     int actual = main.add(2, 3);
-    // System.out.println(start() + "add(5, 0) " + "actual: " + actual + " expected: " + expected +
-    // compare(expected == actual));
     assertEquals(expected, actual);
   }
 
@@ -41,8 +18,12 @@ public class MainTest {
     Main main = new Main();
     int expected = 5;
     int actual = main.subtract(10, 5);
-    // System.out.println(start() + "subtract(10, 5) " + "actual: " + actual + " expected: " +
-    // expected + compare(expected == actual));
     assertEquals(expected, actual);
   }
+
+  @Test
+  public void testMain() {
+    Main.main(new String[] {});
+  }
+
 }
