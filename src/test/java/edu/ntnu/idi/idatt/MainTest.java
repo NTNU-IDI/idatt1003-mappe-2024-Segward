40c82712f8a348f9bca115d75e41cae984f9375d
Segward <gustask@stud.ntnu.no>
2024-10-17 14:32:28 +0200

    Minor changes

--- src/test/java/edu/ntnu/idi/idatt/MainTest.java
+++ src/test/java/edu/ntnu/idi/idatt/MainTest.java
@@ -1,29 +1,9 @@
 package edu.ntnu.idi.idatt;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
-
-  @Test
-  public void testAdd() {
-    Main main = new Main();
-    int expected = 5;
-    int actual = main.add(2, 3);
-    assertEquals(expected, actual);
-  }
-
-  @Test
-  public void testSubtract() {
-    Main main = new Main();
-    int expected = 5;
-    int actual = main.subtract(10, 5);
-    assertEquals(expected, actual);
-  }
-
-  @Test
-  public void testMain() {
-    Main.main(new String[] {});
-  }
 
 }
