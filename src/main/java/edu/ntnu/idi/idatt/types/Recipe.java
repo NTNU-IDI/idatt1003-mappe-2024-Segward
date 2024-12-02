2beb0ffbfc6ccd4e94463a67770f67690bd96a1c
Segward <gustask@stud.ntnu.no>
2024-12-02 07:30:28 +0100

    Implemented methods and fields of recipe class

--- src/main/java/edu/ntnu/idi/idatt/types/Recipe.java
+++ src/main/java/edu/ntnu/idi/idatt/types/Recipe.java
@@ -1,7 +1,48 @@
 package edu.ntnu.idi.idatt.types;
 
+import java.util.ArrayList;
+
 /**
- * @version 1.0
+ * @version 1.1
  * @since 1.0
  */
-public class Recipe {}
+public class Recipe {
+  private ArrayList<Grocery> ingredients;
+  private String name;
+  private String description;
+
+  /**
+   * Creates a new recipe with the specified name, description and ingredients.
+   *
+   * @param name the name of the recipe
+   * @param description the description of the recipe
+   * @param ingredients the ingredients of the recipe
+   */
+  public Recipe(String name, String description, ArrayList<Grocery> ingredients) {
+    this.ingredients = ingredients;
+    this.name = name;
+    this.description = description;
+  }
+
+  /**
+   * Getter for a formatted string of the recipe.
+   *
+   * @return a formatted string of the recipe
+   */
+  public String getFormattedString() {
+    StringBuilder sb = new StringBuilder();
+    for (Grocery ingredient : ingredients) {
+      sb.append(ingredient.getFormattedString());
+    }
+    return name + ": " + description + "\n" + sb.toString();
+  }
+
+  /**
+   * Returns the ingredients of the recipe.
+   *
+   * @return the ingredients of the recipe
+   */
+  public ArrayList<Grocery> getIngredients() {
+    return ingredients;
+  }
+}
